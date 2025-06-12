7d2b9a81ffa7a6311de9c099e6b780d7a7e8b6d7
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-06-12 16:12:45 +0800

    Initial Commit

--- frontend/src/components/common/AdminSidebar.jsx
+++ frontend/src/components/common/AdminSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouseUser, faUsers, faCalendar, faChartSimple, faFile, faBell, faRightFromBracket, faGear, faBars } from '@fortawesome/free-solid-svg-icons';
 
