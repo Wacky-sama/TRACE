80ecf37741b600ac2b7f545fa66df859b117124f
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-10-12 23:20:55 +0800

    Refactor model imports to correct paths and add new User, Event, and ActivityLog models with relationships and fields for enhanced functionality.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,8 +1,8 @@
 from app.database import Base
 
 # Import all models so Alembic and SQLAlchemy see them
-from app.models.user_models import User
-from app.models.event_models import Event
+from backend.app.models.users_models import User
+from backend.app.models.events_models import Event
 from app.models.gts_responses_models import GTSResponse
 from app.models.event_attendance_models import EventAttendance
-from app.models.activity_log_models import ActivityLog
+from backend.app.models.activity_logs_models import ActivityLog
