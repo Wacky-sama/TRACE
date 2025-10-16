75edd9ffafcd26fb6ba1f70daa6fbebbd1abc46c
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-10-16 20:46:25 +0800

    added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { UserProvider } from './context/UserContext.jsx'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { UserProvider } from "./context/UserContext.jsx";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <ThemeProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ThemeProvider>
   </StrictMode>
-)
+);
