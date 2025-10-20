c7b4a462eaff19a48eaf3b44353a86c11acd6eac
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-10-20 21:00:36 +0800

    updated

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,6 +3,6 @@ from app.database import Base
 # Import all models so Alembic and SQLAlchemy see them
 from app.models.users_models import User
 from app.models.events_models import Event
-from app.models.gts_responses_models import GTSResponse
+from app.models.gts_responses_models import GTSResponses
 from app.models.event_attendance_models import EventAttendance
 from app.models.activity_logs_models import ActivityLog
