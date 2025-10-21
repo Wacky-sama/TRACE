c9068c86206694f795bccfb9265a9bada91e743c
Joshua-Xed <tagubaphilipjoshuav@gmail.com>
2025-10-21 17:14:09 +0800

    fix: imports

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -1,5 +1,5 @@
 import jwt
-from jwt import PyJWTError as JWTError
+from jwt.exceptions import PyJWTError as JWTError
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
@@ -17,8 +17,12 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     )
 
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        username: str = payload.get("sub")  # sub = username in your token
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+        username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
     except JWTError:
