5be30d1e41b6522571dec15c07d4ee62cb40456f
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-10-28 16:08:45 +0800

    notifications

--- backend/app/schemas/activity_logs_schemas.py
+++ backend/app/schemas/activity_logs_schemas.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel
+from enum import Enum
+from pydantic import BaseModel # pyright: ignore[reportMissingImports]
 from typing import Any, Optional 
 from datetime import datetime, timezone
 from uuid import UUID
 
+class NotificationTYpe(str, Enum):
+    alert = "alert"
+    message = "message"
+    update = "update"
 class ActivityLogBase(BaseModel):
     action_type: str
     description: str
@@ -14,7 +19,12 @@ class ActivityLogCreate(ActivityLogBase):
 
 class ActivityLogResponse(ActivityLogBase):
     id: UUID
+    action_type: str
+    description: Any
     user_id: UUID
+    target_user_id: Optional[UUID] = None
+    meta_data: Optional[Any] = None
+    is_read: Optional[bool] = False
     created_at: datetime
 
     class Config:
