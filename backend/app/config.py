c1416b0b5f84e2aa60d8dcffa927a5cf48bd1385
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-11-06 21:54:14 +0800

    feat: added toast for token expiration

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from typing import List
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     DB_HOST: str
     DB_PORT: str
